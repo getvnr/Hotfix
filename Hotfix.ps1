b65e218960006b295f20f9b3324b73e9e0cfe015
getvnr <getvnr@gmail.com>
2017-03-21 16:10:20 +0530

    Add files via upload

---
+++ Hotfix.ps1
@@ -0,0 +1,8 @@
+﻿$job = Test-connection -ComputerName (Get-Content c:\dump\Servers.txt) -asjob
+    PS C:\>if ($job.JobStateInfo.State -ne "Running") {$Results = Receive-Job $job}
+
+	
+#get-hotfix -computername (Get-Content $pwd\servers.txt) | Export-Csv $pwd\Output.csv
+
+#Get-HotFix |
+#Select-Object Description,HotFixID,InstalledBy,InstalledOn
